ba2a3f108462c4c28f011d6d096c4f3d8db7b518
byenet <nhathuynguyenho@gmail.com>
2020-12-03 13:58:21 +0700

    upload

--- src/main/java/com/ktltat/vegetshop/api/SanPhamAPI.java
+++ src/main/java/com/ktltat/vegetshop/api/SanPhamAPI.java
@@ -131,10 +131,85 @@ public class SanPhamAPI {
         return sanPhamService.addSanPham(sanPhamDTO);
     }
 
-    @PutMapping(value = "/sanpham")
+//    @PutMapping(value = "/sanpham")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    public void updateSanPham(@RequestBody SanPhamDTO sanPhamDTO){
+//        sanPhamService.updateSanPham(sanPhamDTO);
+//    }
+    @PutMapping(value = "/uploadsp")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public void updateSanPham(@RequestBody SanPhamDTO sanPhamDTO){
-        sanPhamService.updateSanPham(sanPhamDTO);
+    public @ResponseBody ResponseEntity<?> updateSanPham(@RequestParam("idsp") int idsp,@RequestParam("tensp") String tensp,
+                                                      @RequestParam("giasp") String giasp, @RequestParam("thuonghieu") String thuonghieu
+                                                    , @RequestParam("tinhtrang") String tinhtrang, @RequestParam("nguongoc") String nguongoc
+                                                    , @RequestParam("khoiluong") String khoiluong
+                                                    , @RequestParam("chitiet") String chitiet
+                                                    //, @RequestParam("loaisp") String loaisp
+                                                    , final @RequestParam("hinhanh") MultipartFile hinhanh){
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            String fileName = hinhanh.getOriginalFilename();
+            String filePath = Paths.get(uploadDirectory, fileName).toString();
+            long size = hinhanh.getSize();
+
+            // Save the file locally
+            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(filePath)));
+            stream.write(hinhanh.getBytes());
+            stream.close();
+
+            SanPhamEntity sanPhamEntity = new SanPhamEntity();
+            sanPhamEntity.setIdsp(idsp);
+            sanPhamEntity.setTensp(tensp);
+            sanPhamEntity.setKhoiluong(khoiluong);
+            sanPhamEntity.setNguongoc(nguongoc);
+            sanPhamEntity.setGiasp(giasp);
+            sanPhamEntity.setThuonghieu(thuonghieu);
+            sanPhamEntity.setTinhtrang(tinhtrang);
+            sanPhamEntity.setChitiet(chitiet);
+            sanPhamEntity.setHinhanh(filePath);
+            sanPhamService.saveSanPham(sanPhamEntity);
+//            log.info("Employee Created");
+            headers.add("Employe Saved With Image - ", fileName);
+            List<SanPhamDTO> sanPhamDTOS = sanPhamService.findAllSanPham();
+//            int sizeArr = sanPhamDTOS.size();
+            //Xử lí loại thêm vào:
+//            String[] arrayList = loaisp.split("-");
+//            for (String x: arrayList) {
+//                if (x.equals("1")){
+//                    PhanLoaiDTO phanLoaiDTO = new PhanLoaiDTO();
+//                    phanLoaiDTO.setIdloaisp(Integer.parseInt(x));
+//                    phanLoaiDTO.setIdsp(sanPhamDTOS.get(sizeArr-1).getIdsp());
+//                    phanLoaiService.addPhanLoai(phanLoaiDTO);
+//                } else if (x.equals("2")){
+//                    PhanLoaiDTO phanLoaiDTO = new PhanLoaiDTO();
+//                    phanLoaiDTO.setIdloaisp(Integer.parseInt(x));
+//                    phanLoaiDTO.setIdsp(sanPhamDTOS.get(sizeArr-1).getIdsp());
+//                    phanLoaiService.addPhanLoai(phanLoaiDTO);
+//                } else if (x.equals("3")){
+//                    PhanLoaiDTO phanLoaiDTO = new PhanLoaiDTO();
+//                    phanLoaiDTO.setIdloaisp(Integer.parseInt(x));
+//                    phanLoaiDTO.setIdsp(sanPhamDTOS.get(sizeArr-1).getIdsp());
+//                    phanLoaiService.addPhanLoai(phanLoaiDTO);
+//                } else if (x.equals("4")){
+//                    PhanLoaiDTO phanLoaiDTO = new PhanLoaiDTO();
+//                    phanLoaiDTO.setIdloaisp(Integer.parseInt(x));
+//                    phanLoaiDTO.setIdsp(sanPhamDTOS.get(sizeArr-1).getIdsp());
+//                    phanLoaiService.addPhanLoai(phanLoaiDTO);
+//                }else if (x.equals("5")){
+//                    PhanLoaiDTO phanLoaiDTO = new PhanLoaiDTO();
+//                    phanLoaiDTO.setIdloaisp(Integer.parseInt(x));
+//                    phanLoaiDTO.setIdsp(sanPhamDTOS.get(sizeArr-1).getIdsp());
+//                    phanLoaiService.addPhanLoai(phanLoaiDTO);
+//                }
+//            }
+
+            return new ResponseEntity<>(filePath+" Employe Saved With File - " + fileName, headers, HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("Exception: " + e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @DeleteMapping(value = "/xoasanpham={idsp}")
